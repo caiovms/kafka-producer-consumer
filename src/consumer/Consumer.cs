12007008f56b1e5ac7a165cdf7b73904bd1cb423
caio.silva <caio.silva@WRNPOA068>
2020-07-21 11:06:29 -0300

    Initial Commit

---
+++ src/consumer/Consumer.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace consumer
+{
+    class Consumer
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "127.0.0.1:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var builder = new ConsumerBuilder<Ignore, string>(configuration).Build();
+            {
+                builder.Subscribe("test-topic");
+                
+                var cancellationToken = new CancellationTokenSource();
+
+                Console.CancelKeyPress += (_, e) => { e.Cancel = true; cancellationToken.Cancel(); };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumerResult = builder.Consume(cancellationToken.Token);
+                            Console.WriteLine($"Consumed message '{consumerResult.Value}' at: '{consumerResult.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    builder.Close();
+                }
+            }
+        }
+    }
+}
---
+++ src/producer/Producer.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace producer
+{
+    class Producer
+    {
+        public static async Task Main()
+        {
+            var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
+
+            using var builder = new ProducerBuilder<Null, string>(config).Build();
+            {
+                try
+                {
+                    
+                    var count = 0;
+
+                    while (true)
+                    {
+                        var deliveryResult = await builder.ProduceAsync("test-topic", new Message<Null, string> { Value = $"test: {count++}" });
+
+                        Console.WriteLine($"Delivered '{deliveryResult.Value}' to '{deliveryResult.TopicPartitionOffset} | {count}'");
+
+                        Thread.Sleep(2000);
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
